d59604d3ddf1dd93c82aaa111e1ed2550635f637
Junior Oliveira <junior.getsoftware@gmail.com>
2018-06-05 17:59:15 -0300

    binding de propriedadescustomizadas com input

--- ang-binding/src/app/app.module.ts
+++ ang-binding/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuncionarioCardComponent
   ],
   imports: [
     BrowserModule, FormsModule
